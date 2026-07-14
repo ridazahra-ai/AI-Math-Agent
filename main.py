01c1e5093a97db96a0a58db9e32a0c34209fc224
Rida Zahra <ridaz200512@gmail.com>
2026-07-14 16:06:06 +0500

    update project

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import streamlit as st
+import os
 from agent import chat
 
 st.set_page_config(page_title = "AI Math Agrent", page_icon = "🧮", layout = "centered")
 st.title("🧮 AI Math Agent")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY") or st.secrets["GOOGLE_API_KEY"]
 
 print("\nType exit or quit for exiting from this...")
 while True:
